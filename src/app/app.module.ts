66b8c53cb89f706f76ee23bd3cfaa65803faf02a
shanhong cheng <shanhong.cheng@utah.edu>
2017-11-14 15:25:24 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    CoursesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses-list/courses-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import * as data from '../../courses.json';
+@Component({
+  selector: 'courses-list',
+  templateUrl: 'courses-list.component.html',
+  styles: []
+})
+export class CoursesListComponent implements OnInit {
+  coursesObservable: Observable<any[]>;
+  constructor() { }
+  ngOnInit() {
+    this.coursesObservable = this.getCourses('/courses');
+  }
+  getCourses(listPath): Observable<any[]> {
+    let courses: any[] = Object.keys(data.courses).map( key => data.courses[key]);
+    return Observable.of(courses);
+  }
+}
