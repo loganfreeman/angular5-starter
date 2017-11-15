3a36be25b63ff0a4adf5d7c93f6940d76139dbc1
shanhong cheng <shanhong.cheng@utah.edu>
2017-11-15 15:00:46 -0700

    no need module for hero

--- src/app/hero/hero.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule }             from '@angular/core';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
-
-@NgModule({
-  declarations: [
-    HeroDetailComponent
-  ]
-})
-export class HeroModule { }
